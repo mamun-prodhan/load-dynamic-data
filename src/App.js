5cd92d73d94e02ec722532816dee33d0926bca1c
mamun-prodhan <mamunpr9@gmail.com>
2023-06-13 11:30:48 +0600

    dynamic data load

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, loadUsers] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => loadUsers(data))
+  },[])
+
+  console.log(users);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        users.map(user => <LoadUsers key={user.id} name={user.name}></LoadUsers>)
+      }
     </div>
   );
+}
+
+function LoadUsers(props){
+  return(
+    <div className='userCard'>
+      <h2>Name of Users: {props.name}</h2>
+    </div>
+  )
 }
 
 export default App;
